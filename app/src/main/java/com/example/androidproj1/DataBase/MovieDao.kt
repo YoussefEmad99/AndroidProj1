cfd4fd20362c085715b51ef1d094afe1e592604e
daniella <daniellabotros@gmail.com>
2020-09-09 05:45:11 +0200

    Movie Dao Interface

--- app/src/main/java/com/example/androidproj1/DataBase/MovieDao.kt
+++ app/src/main/java/com/example/androidproj1/DataBase/MovieDao.kt
@@ -1,4 +1,17 @@
 package com.example.androidproj1.DataBase
 
-class MovieDao {
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.example.androidproj1.Models.UI.Movie
+
+@Dao
+interface MovieDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun addWeather(movie:Movie)
+
+    @Query("SELECT * FROM Movie_Table")
+    fun getWeather(): Movie
+
 }

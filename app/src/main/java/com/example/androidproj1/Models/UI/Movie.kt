e1054711639b93714a56f82ff301ee476d1724ce
daniella <daniellabotros@gmail.com>
2020-09-09 05:41:06 +0200

    Movie data class

--- app/src/main/java/com/example/androidproj1/Models/UI/Movie.kt
+++ app/src/main/java/com/example/androidproj1/Models/UI/Movie.kt
@@ -1,4 +1,10 @@
 package com.example.androidproj1.Models.UI
 
-class Movie {
-}
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "Movie_Table")
+data class Movie (@PrimaryKey(autoGenerate = true) val id:Int,
+                  val title:String,
+                  val popularity:Double,
+                  val discription:String)

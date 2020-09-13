bd4f863d007c2ff7db84ffa8b00494f166dce10a
YoussefEmad99 <insipidguy99@yahoo.com>
2020-09-13 08:00:06 +0200

    inserted the recycler view inside fragmentA, nav host doesnt work yet

---
+++ app/src/main/java/com/example/androidproj1/fragments/FragmentA.kt
@@ -0,0 +1,26 @@
+package com.example.androidproj1.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.androidproj1.R
+
+class FragmentA : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_a, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+//        fragmentA_title.setOnClickListener {
+//            findNavController().navigate(R.id.action_fragmentA_to_fragmentB2)
+//        }
+    }
+
+}
